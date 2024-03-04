06aec6dd5fd4d2f04b7ae0223d43c0c4d5f13621
mrblocken <114803711+mrblocken@users.noreply.github.com>
2024-03-04 14:54:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import machine, utime, _thread
 from machine import Pin
-
+ 
 left_sensor = machine.ADC(2)
 middle_sensor = machine.ADC(1)
 right_sensor = machine.ADC(0)
@@ -12,29 +12,24 @@ L_motor_forw = machine.PWM(machine.Pin(1))
 L_motor_forw.freq(1000)
 L_motor_back = machine.PWM(machine.Pin(2))
 L_motor_back.freq(1000)
-led = Pin("LED", Pin.OUT)
 speed=65000
- 
 backspeed = int(speed*0.3)
 R_motor_back.duty_u16(0) 
 L_motor_back.duty_u16(0)
- 
 def forward ():
     L_motor_forw.duty_u16(speed)    
     R_motor_forw.duty_u16(speed)
- 
 def stop ():
     L_motor_forw.duty_u16(0)
     R_motor_forw.duty_u16(0)
-
+ 
 def right_turn ():
     L_motor_forw.duty_u16(speed)
     R_motor_forw.duty_u16(backspeed)
 def left_turn ():
     L_motor_forw.duty_u16(backspeed)
     R_motor_forw.duty_u16(speed)
- 
- 
+
 global start_pressed
 start_pressed = 0
 def start_reader_thread():
@@ -51,9 +46,9 @@ def start_reader_thread():
                 start_pressed = 0
 #                print("Knapp False")
                 utime.sleep(2)
- 
 _thread.start_new_thread(start_reader_thread, ())
 sensor_limit = 20000
+ 
 while True:
     if start_pressed == 1:
         left_sens = left_sensor.read_u16()
@@ -62,22 +57,16 @@ while True:
 #         utime.sleep(0.01)
 #         led.value(1)
 #         print(left_sens, middle_sens, right_sens)
+        if ((left_sens < sensor_limit) and (right_sens > sensor_limit)): #hade det fungerat om man bara hade en check per variable? exempel left_sensor > sensor_limit?
+            right_turn()
  
-        if ((left_sens < sensor_limit) and (middle_sens > sensor_limit) and (right_sens < sensor_limit)):
-            forward()
-
-        elif ((left_sens < sensor_limit) and (right_sens > sensor_limit)):
-            right_turn() 
-
         elif ((left_sens > sensor_limit) and (right_sens < sensor_limit)):
             left_turn()
-
-#         elif ((left_sens < sensor_limit) and (middle_sens < sensor_limit) and (right_sens < sensor_limit)):
-#             backward()
-            
         elif ((left_sens > sensor_limit) and (middle_sens > sensor_limit) and (right_sens > sensor_limit)):
-              stop()
-    elif start_pressed == 0:
+               stop()   #behövs denna?
+ 
+        elif (middle_sens > sensor_limit):
+            forward()
+elif start_pressed == 0:
         stop()
         utime.sleep(0.05)
-        led.value(0)
